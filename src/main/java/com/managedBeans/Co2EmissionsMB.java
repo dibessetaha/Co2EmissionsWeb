03449a55c55dc91ebb5a73d0346e736763747f8c
dibessetaha <dibessetaha@gmail.com>
2023-12-02 00:15:45 +0100

    Adding xhtml files

--- src/main/java/com/managedBeans/Co2EmissionsMB.java
+++ src/main/java/com/managedBeans/Co2EmissionsMB.java
@@ -3,8 +3,10 @@ package com.managedBeans;
 import java.io.Serializable;
 import java.util.List;
 
+import com.beans.Co2DataFromExcel;
 import com.dao.ICo2Emissions;
 import com.entities.Co2Emission;
+import com.entities.DataScientist;
 
 import jakarta.ejb.EJB;
 import jakarta.enterprise.context.SessionScoped;
@@ -16,6 +18,7 @@ public class Co2EmissionsMB implements Serializable {
 	
 
 	private static final long serialVersionUID = 1L;
+	public static boolean publish = false ; 
 
 	@EJB
 	private ICo2Emissions metier ; 
@@ -25,10 +28,15 @@ public class Co2EmissionsMB implements Serializable {
 	public String saveCo2EmissionData() {
 		metier.addCo2Emisson(co2Emission);
 		return "co2Emissions" ; 
-	}
+	} 
 	
 	public List<Co2Emission> getListCo2Emissions(){
+//		publishDS();
 		return metier.getCo2Emissions() ; 
+	}
+	
+	public List<Co2Emission> getListCo2EmissionsPerScientist(DataScientist ds){
+		return metier.getCo2EmissionsPerDs(ds) ; 
 	}
 	
 	public Co2Emission getCo2Emission() {
@@ -41,6 +49,11 @@ public class Co2EmissionsMB implements Serializable {
 	
 	public int getTotalData() {
 		return metier.getCo2Emissions().size() ; 
+	}
+	
+	private void publishDS() {
+		Co2DataFromExcel ds = new Co2DataFromExcel() ; 
+		ds.test();  
 	}
 
 }
---
+++ src/main/java/com/managedBeans/DataScientistMB.java
@@ -0,0 +1,43 @@
+package com.managedBeans;
+
+import java.io.Serializable;
+
+import com.dao.ICo2Emissions;
+import com.dao.IDataScientist;
+import com.entities.DataScientist;
+import com.oracle.wls.shaded.org.apache.xalan.xsltc.compiler.sym;
+
+import jakarta.ejb.EJB;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+
+@Named("dataScientistMB")
+@SessionScoped
+public class DataScientistMB implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@EJB
+	private IDataScientist metier ; 
+	
+	DataScientist dataScientist = new DataScientist() ; 
+	
+	public String login() {
+		if(metier.login(dataScientist.getUsername(), dataScientist.getPassword())) {
+			return "co2Emissions" ; 
+
+		}else {
+			return "login" ; 
+		}
+		
+	}
+	
+	public DataScientist getDataScientist() {
+		return dataScientist;
+	}
+	
+
+
+}
